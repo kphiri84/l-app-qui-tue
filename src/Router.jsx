282127207d6adeae7fad7e18e8c75ea3af98a2c9
REAU <anaisreau@Anais.Reau>
2020-04-30 12:12:59 +0200

    pourri

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,7 @@ import { Switch,Route } from 'react-router-dom'
 import Home from './components/Home'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import Search from './components/Search'
+import Envie from './components/Envie'
 
 class Router extends React.Component {
 render(){
@@ -13,7 +13,7 @@ return (
 <NavBar />
 <Switch>
 <Route exact path = "/" component={Home} />
-<Route path= "/Recherche" component={Search} />
+<Route path= "/Envie" component={Envie} />
 </Switch>   
 <Footer />
 </>
